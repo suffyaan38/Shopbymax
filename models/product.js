230932ffe08f6dad809916e00fcdc7c2dd99b1a4
suffyaan38 <136230476+suffyaan38@users.noreply.github.com>
2023-10-06 21:24:29 +0530

    using sequelize

--- models/product.js
+++ models/product.js
@@ -1,35 +1,61 @@
-const db=require('../util/database');
+// const db=require('../util/database');
 
 
-const Cart = require('./cart');
+// const Cart = require('./cart');
 
 
 
 
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+// module.exports = class Product {
+//   constructor(id, title, imageUrl, description, price) {
+//     this.id = id;
+//     this.title = title;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this.price = price;
+//   }
+
+//   save() {
+//     return db.execute('insert into products (title, price,imageUrl, description) values(?,?,?,?)',
+//     [this.title,this.price,this.imageUrl,this.description])
+//   }
+
+//   static deleteById(id) {
+//     return db.execute('delete from products where id=?',[id]);
+//   }
+
+//   static fetchAll() {
+//     return db.execute('select * from Products');
+//   }
+
+//   static findById(id) {
+//     return db.execute('select * from products where products.id=?',[id]);
+//   }
+// };
+
+const Sequelize=require('sequelize');
+const sequelize=require('../util/database');
+const Product=sequelize.define('product',{
+  id:{
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull:false,
+    primaryKey:true
+  },
+  title: Sequelize.STRING,
+  price:{
+    type:Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+  description:{
+    type:Sequelize.STRING,
+    allowNull:false
   }
+});
 
-  save() {
-    return db.execute('insert into products (title, price,imageUrl, description) values(?,?,?,?)',
-    [this.title,this.price,this.imageUrl,this.description])
-  }
-
-  static deleteById(id) {
-    return db.execute('delete from products where id=?',[id]);
-  }
-
-  static fetchAll() {
-    return db.execute('select * from Products');
-  }
-
-  static findById(id) {
-    return db.execute('select * from products where products.id=?',[id]);
-  }
-};
+module.exports=Product;
--- util/database.js
+++ util/database.js
@@ -1,9 +1,17 @@
-const mysql=require('mysql2');
-const pool=mysql.createPool({
-    host: 'localhost',
-    user:'root',
-    database: 'nodecomplete',
-    password:'password'
+// const mysql=require('mysql2');
+// const pool=mysql.createPool({
+//     host: 'localhost',
+//     user:'root',
+//     database: 'nodecomplete',
+//     password:'password'
+// });
+
+// module.exports=pool.promise();
+
+const Sequelize=require('sequelize');
+const sequelize=new Sequelize('nodecomplete','root','password',
+{dialect:'mysql', 
+host:'localhost'
 });
 
-module.exports=pool.promise();
+module.exports=sequelize;
